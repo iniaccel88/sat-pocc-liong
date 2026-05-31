2f8eb115d59b55a2e8642ecf0e68cf0377987f78
Liong <axel.tanzil@student.citrakasih.sch.id>
2026-05-31 20:14:39 +0800

    final design

---
+++ PoCC_SAT_API/src/App.tsx
@@ -0,0 +1,5 @@
+import Liong from "./pages/API_SAT";
+import './App.css';
+function App() {
+  return <Liong/>;
+} export default App
---
+++ PoCC_SAT_API/src/pages/API_SAT.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+interface JobResult {
+    thumbnail: string;
+    title: string;
+    company_name: string;
+    detected_extensions: {
+        salary: string;
+    };
+    location: string;
+}
+
+interface SerpApiResponse {
+    jobs_results: JobResult[];
+}
+
+function Liong() { // Liong 
+    const [data, setData] = useState<SerpApiResponse | null>(null);
+    const params = {
+        engine: "google_jobs",
+        q: "firefighter", // bagian q bisa diganti terserah
+        hl: "en",
+    }
+    
+    useEffect(() => {
+        const queryString = new URLSearchParams({ ...params, api_key: API_KEY }).toString();
+        const serpUrl = `${ENDPOINT}?${queryString}`;
+        fetch(`api/search?${queryString}`)
+            .then(response => response.json())
+            .then((result: SerpApiResponse) => {
+                console.log(result);
+                setData(result);
+            })
+            .catch((err) => console.error('Error:', err));
+    }, []);
+
+    if (!data) {
+        return <h1>Loading...</h1>;
+    }
+
+    return (
+        <div>
+            <img src={data.jobs_results[1].thumbnail} width="100"/>
+            <h2>{data.jobs_results[1].title}</h2>
+            <p>{data.jobs_results[1].company_name}</p>
+            <p>${data.jobs_results[1].detected_extensions.salary}</p>
+            <p>{data.jobs_results[1].location}</p>
+        </div>
+    )
+} export default Liong;
